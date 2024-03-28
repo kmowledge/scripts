59b3dfe34960eda1f3d2dc0e8abc8b103c0f1bb1
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-03-28 19:01:57 +0100

    fixed deadlock -added loop in receiving func; added ANSI escape

--- wristWatch/drafts/dynamicUpdateTest.go
+++ wristWatch/drafts/dynamicUpdateTest.go
@@ -5,21 +5,23 @@ import (
 	"os"
 )
 
-func switchPlayer(usrInput chan string, j, k, l *int) {
-	pl := <-usrInput
-	switch pl {
-	case "j":
-		*j++
-		fmt.Printf("Jeremi: %v", j) //po value\033[2K
-	case "k":
-		*k++
-		fmt.Fprintf(os.Stdout, "Kacper %v\r", k) //input nadpisuje output, wysłanie inputa powoduje newline
-	case "l":
-		*l++
-		fmt.Printf("\rLinda: %v\r", l)
-		// fmt.Fprintf(os.Stdout, "\r")
-	default:
-		fmt.Printf("\rwrong input")
+func switchPlayer(plCh <-chan string, j, k, l *int) {
+	for {
+		pl := <-plCh
+		switch pl {
+		case "j":
+			*j++
+			fmt.Printf("\033[A\033[2K Jeremi: %v\033[B", *j) //po value\033[2K
+		case "k":
+			*k++
+			fmt.Fprintf(os.Stdout, "Kacper %v\r", *k) //input nadpisuje output, wysłanie inputa powoduje newline
+		case "l":
+			*l++
+			fmt.Printf("\rLinda: %v\r", *l)
+			// fmt.Fprintf(os.Stdout, "\r")
+		default:
+			fmt.Printf("\rwrong input")
+		}
 	}
 }
 
@@ -30,9 +32,13 @@ func main() {
 	var i string
 	fmt.Println("send one of the letters: j k l")
 	plCh := make(chan string)
+
+	go switchPlayer(plCh, &j, &k, &l)
+
 	for i != "exit" {
 		fmt.Scan(&i)
 		plCh <- i
-		go switchPlayer(plCh, &j, &k, &l)
 	}
 }
+
+// 			fmt.Printf("Jeremi: %v\nKacper: %v\nLinda: %v\n", *j, *k, *l)
