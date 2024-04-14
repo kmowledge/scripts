c601e0755c2ba68629dfcb8796c1a265b97c934c
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-04-14 12:01:52 +0200

    -> opens websites in new tabs

--- www.py
+++ www.py
@@ -2,6 +2,8 @@
 
 import webbrowser, sys, time
 
+arg = '' # without this->[xxxxx:xxxxx:0100/000000.xxxxxx:ERROR:zygote_linux.cc(672)] write: Broken pipe (32)
+
 for arg in sys.argv[1:]:
       url = ['https://', arg, '/']
       webbrowser.open(''.join(url))
